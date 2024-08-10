0c0551c2f22299f22d35d9f73a9f4639494b1150
sodam <damsoshin.dev@gmail.com>
2024-08-10 11:47:37 +0900

    feat: 배열, 객체 복사 구현

--- deepcopy.js
+++ deepcopy.js
@@ -2,7 +2,16 @@ function deepcopy(value) {
   if (value === null || typeof value !== "object") {
     return value;
   }
-  throw new Error("얕은 복사만 구현되어 있음");
+
+  if (Array.isArray(value)) {
+    return value.map(deepcopy);
+  }
+
+  const objCopy = {};
+  Object.keys(value).forEach((key) => {
+    objCopy[key] = deepcopy(value[key]);
+  });
+  return objCopy;
 }
 
 module.exports = deepcopy;
