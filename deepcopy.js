de9214ff6676365c0b5289520a91ec7b1d873d89
sodam <damsoshin.dev@gmail.com>
2024-08-11 19:29:37 +0900

    feat: deepcopy 함수에 Set, Map 타입 복사 기능 추가

--- deepcopy.js
+++ deepcopy.js
@@ -7,6 +7,22 @@ function deepcopy(value) {
     return value.map(deepcopy);
   }
 
+  if (value instanceof Set) {
+    const setCopy = new Set();
+    value.forEach((item) => {
+      setCopy.add(deepcopy(item));
+    });
+    return setCopy;
+  }
+
+  if (value instanceof Map) {
+    const mapCopy = new Map();
+    value.forEach((val, key) => {
+      mapCopy.set(key, deepcopy(val));
+    });
+    return mapCopy;
+  }
+
   const objCopy = {};
   Object.keys(value).forEach((key) => {
     objCopy[key] = deepcopy(value[key]);
